6fde1272b80b21ca86ed39309a697d544f165ad6
kou-kawam24 <sora.kk0724@gmail.com>
2021-01-02 10:47:23 +0900

    入力文字制限のバリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence:true
-  validates :last_name_kana, presence:true
-  validates :first_name_kana, presence:true
-  validates :birthday, presence:true
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    validates :birthday
+
+    with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' } do
+      validates :password
+    end
   
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width katakana characters' } do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end 
 end
