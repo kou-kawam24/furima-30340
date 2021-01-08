30742a291e85e8fb93b78775946e6ef13e43d2ed
kou-kawam24 <sora.kk0724@gmail.com>
2021-01-08 16:56:20 +0900

    createアクションの定義と新規投稿画面の編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :image,  presence: true
+  validates :title,  presence: true
+  validates :text,   presence: true
+  validates :price,  presence: true
 end
