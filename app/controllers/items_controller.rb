586c2b3f554d18fa914f5f834cbb6171d6569033
kou-kawam24 <sora.kk0724@gmail.com>
2021-01-10 11:27:02 +0900

    ActiveHashの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :text, :price, :image)
+    params.require(:item).permit(:title, :text, :price, :image, :category_id, :condition_id, :shipping_fee_id, :shipping_area_id, :ship_date_id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :shipping_area
+  belongs_to :ship_date
   belongs_to :user
   has_one_attached :image
 
@@ -6,4 +12,9 @@ class Item < ApplicationRecord
   validates :title,  presence: true
   validates :text,   presence: true
   validates :price,  presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_fee_id, numericality: { other_than: 1 }
+  validates :shipping_area_id, numericality: { other_than: 0 }
+  validates :ship_date_id, numericality: { other_than: 1 }
 end
