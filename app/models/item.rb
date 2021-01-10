95e14117881ac60be5118d54d60288ff55b91986
kou-kawam24 <sora.kk0724@gmail.com>
2021-01-10 18:52:56 +0900

    出品商品が正しくDBに保存される

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,13 +8,15 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image,  presence: true
-  validates :title,  presence: true
-  validates :text,   presence: true
-  validates :price,  presence: true
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :shipping_fee_id, numericality: { other_than: 1 }
-  validates :shipping_area_id, numericality: { other_than: 0 }
-  validates :ship_date_id, numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :text
+    validates :price, inclusion: { in:300..9999999 }, format: { with: /\A[0-9]+\z/ }
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :shipping_fee_id, numericality: { other_than: 1 }
+    validates :shipping_area_id, numericality: { other_than: 0 }
+    validates :ship_date_id, numericality: { other_than: 1 }
+  end
 end
