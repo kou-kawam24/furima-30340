9110c890c539413724aae9e37cc802a30f1a2416
kou-kawam24 <sora.kk0724@gmail.com>
2021-01-15 22:27:25 +0900

    itemモデルの単体テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,8 @@ class Item < ApplicationRecord
     validates :image
     validates :title
     validates :text
-    validates :price, inclusion: { in: 300..9999999, message: 'Out of setting range' }, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+    validates :price, numericality: { only_integer: true, message: 'Half-width number' }
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range' }
     validates :category_id, numericality: { other_than: 1, message: 'Select' }
     validates :condition_id, numericality: { other_than: 1, message: 'Select' }
     validates :shipping_fee_id, numericality: { other_than: 1, message: 'Select' }
