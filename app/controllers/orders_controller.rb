bab7767bfbc6efcac7f4d5e000c0362bddf559b0
kou-kawam24 <sora.kk0724@gmail.com>
2021-02-07 16:25:22 +0900

    ログアウト状態のユーザーや自身の出品した商品の購入ページに遷移できないように修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :move_to_top
+
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if  @order_address.valid?
       pay_item
@@ -30,4 +32,13 @@ class OrdersController < ApplicationController
       currency: 'jpy'
     )
   end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_top
+    redirect_to root_path if current_user == @item.user
+  end
+
 end
