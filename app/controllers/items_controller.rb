20cae14cd8c151d1b080f2ecfe301933505cb2fc
kou-kawam24 <sora.kk0724@gmail.com>
2021-02-12 05:44:48 +0900

    sold outの表示切り替え・売却済み商品の編集無効の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_top, only: [:edit, :update]
+  before_action :move_to_top2, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -56,5 +57,9 @@ class ItemsController < ApplicationController
 
   def move_to_top
     redirect_to root_path unless current_user == @item.user
+  end
+
+  def move_to_top2
+    redirect_to root_path if @item.order.present?
   end
 end
